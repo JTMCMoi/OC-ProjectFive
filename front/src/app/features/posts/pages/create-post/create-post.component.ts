676cc1e5bbbfcb553ae2fad66921fc82d6dbda04
bpbanga <97730466+bpbanga@users.noreply.github.com>
2025-12-19 17:22:45 +0100

    update createcomponent to use currentUser

--- front/src/app/features/posts/pages/create-post/create-post.component.ts
+++ front/src/app/features/posts/pages/create-post/create-post.component.ts
@@ -3,7 +3,9 @@ import { Router } from '@angular/router';
 import { TopicApiService } from 'src/app/features/topics/topic-api.service';
 import { Topic } from 'src/app/shared/models/topics/topic.model';
 import { PostApiService } from '../../post-api.service';
-
+import { AuthService } from 'src/app/core/auth/services/auth.service';
+import { User } from 'src/app/shared/models/user/user.model';
+import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
@@ -14,28 +16,39 @@ export class CreatePostComponent implements OnInit {
   selectedTopicId!: number;
   title = '';
   content = '';
+  user!: User;
 
   constructor(
     private topicService: TopicApiService,
-    private postservice: PostApiService,
-    private router: Router
+    private postService: PostApiService,
+    private router: Router,
+    private authService: AuthService // <-- ajout
   ) {}
 
   ngOnInit(): void {
-    this.topicService.getAll().subscribe(topics => this.topics = topics)
+    // 🔑 Récupération de l'utilisateur connecté
+    this.authService.currentUser$
+      .pipe(filter((user): user is User => !!user))
+      .subscribe(user => this.user = user);
+
+    this.topicService.getAll().subscribe(topics => this.topics = topics);
   }
 
   goBack(): void {
     history.back();
   }
 
-  createPost(): void {
-    if (!this.selectedTopicId || !this.title.trim() || !this.content.trim()) return;
+ createPost(): void {
+  if (!this.selectedTopicId || !this.title.trim() || !this.content.trim()) return;
 
-    this.postservice.create({
-      topicId: this.selectedTopicId,
-      title: this.title,
-      content: this.content
-    }).subscribe(() => this.router.navigate(['/feed']));
-  }
+  this.postService.create({
+    topicId: this.selectedTopicId,
+    title: this.title,
+    content: this.content
+  }).subscribe({
+    next: () => this.router.navigate(['/feed']),
+    error: (err) => alert('Erreur lors de la création de l’article : ' + err.message)
+  });
+}
+
 }
